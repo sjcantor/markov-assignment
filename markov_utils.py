1dfa8ac8d9ab456c01b3becf5a28c44b1a54b73f
Sam Cantor <s.cantor@queensu.ca>
2023-02-26 13:08:52 +0100

    Add util function for chord complexity

--- markov_utils.py
+++ markov_utils.py
@@ -1,7 +1,9 @@
 import numpy as np
+import re
 
 def generate_pdf(event_list):
     'Probability Density Funtion (PDF), for condensing transition tables'
+
     events, counts = np.unique(event_list, return_counts=True, axis=0)
 
     # Tuples can be used for high dimensionality sequences, but for
@@ -11,9 +13,22 @@ def generate_pdf(event_list):
     pdf=counts/sum(counts)
     return events, pdf
 
-def select_event_from_pdf (events, pdf):
+def select_event_from_pdf(events, pdf):
     'Given the next available events and their probabilities, choose one'
+
     event_indexes=range(len(events))
     choice = events[np.random.choice(event_indexes, 1, p=pdf)[0]]
     # print(f'choice: {choice}')
     return choice
+
+def chord_complexity(chord_str):
+    '''
+    Returns an int corresponding to chord complexity.
+    For simplicity, complexity can be evaluated as the number of notes in the chord
+    '''
+    complexity = 1
+    chord_extensions = re.findall(f'\d+', chord_str)
+    for i in chord_extensions:
+        complexity += 1
+    return complexity
+    
