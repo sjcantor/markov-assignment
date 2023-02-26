e6a962de9009b68b318d27ea48ae970e772c4be0
Quoc Duong Nguyen <quoc-duong.nguyen@epita.fr>
2023-02-26 13:52:41 +0100

    Fix random choice probability roundoff error

--- markov_utils.py
+++ markov_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import re
+import random
+
 
 def generate_pdf(event_list):
     'Probability Density Funtion (PDF), for condensing transition tables'
@@ -10,16 +12,19 @@ def generate_pdf(event_list):
     # now it just breaks up my chord strings which I don't like...
     # events=[tuple(x) for x in events]
 
-    pdf=counts/sum(counts)
+    pdf = counts/sum(counts)
     return events, pdf
+
 
 def select_event_from_pdf(events, pdf):
     'Given the next available events and their probabilities, choose one'
 
-    event_indexes=range(len(events))
+    event_indexes = range(len(events))
+    pdf[-1] = 1 - np.sum(pdf[0:-1])
     choice = events[np.random.choice(event_indexes, 1, p=pdf)[0]]
     # print(f'choice: {choice}')
     return choice
+
 
 def chord_complexity(chord_str):
     '''
@@ -27,8 +32,7 @@ def chord_complexity(chord_str):
     For simplicity, complexity can be evaluated as the number of notes in the chord
     '''
     complexity = 1
-    chord_extensions = re.findall(f'\d+', chord_str)
+    chord_extensions = re.findall('\d+', chord_str)
     for i in chord_extensions:
         complexity += 1
     return complexity
-    
